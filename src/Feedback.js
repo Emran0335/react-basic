a013448e3ebbe72c198e80e3a8408f41c99b2458
Emran Hossain <emranhossain0335@gmail.com>
2023-04-25 11:12:39 +0600

    react-basic

--- src/Feedback.js
+++ src/Feedback.js
@@ -46,7 +46,6 @@ const Feedback = () => {
 export default Feedback;
 /*
 We like to send the customers a feedback form. So, let's go ahead and implement a feedback form with react. Note that this form allows the users to provide a score from 0 to 10 and additional comment to tell the chef how delicious the food they enjoyed a few days ago was. So the first step is to implement a control for the score. Here I have used range input for this use case. Range attribute offers two props - one is min and the other is max. I have turned the input a controll component. For this reason, I need state called score that I will initialize to 10 because I know chef's recipe is usually unbeatable. Enabling the user to reduce the score down from 10 when they interact with the slider if they want to. Now in the range input, I have to use the value prop to hook the state up and use onChange to receive the changes and update the state accordingly. Since I also want the numerical score to be displayed with the slider. I am to add that information to this label along with a star. So the user interface or UI is clean and concise.
-
 I want to declare comment area. So I need another state that is called comment. Now I need to hook up the state to the value prop and update the changes via onChange.
 
 Now handleSubmit will check the form validation and after that setComment and setScore should be as usual. This is called good practice.
